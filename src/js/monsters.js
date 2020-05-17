1d9f81e5cf71ee0621659f071b646ccf27c56af6
re-z <re-z@ukr.net>
2020-05-18 02:08:23 +0300

    added pirate monster

--- src/js/monsters.js
+++ src/js/monsters.js
@@ -3,12 +3,29 @@
 import FatPalladinImg from "../assets/monsters/fat-paladin/fat-paladin.png";
 import CursedDollImg from "../assets/monsters/cursed-doll/cursed-doll.png";
 import GladiatorImg from "../assets/monsters/gladiator/gladiator.png";
+import PirateImg from "../assets/monsters/pirate/pirate.png";
+
 // backgrounds
 import burningCastle from "../assets/monsters/fat-paladin/burningCastle.jpg"
 import necroCastle from "../assets/monsters/cursed-doll/necroCastle.jpg"
 import gladiatorArena from "../assets/monsters/gladiator/gladiatorArena.jpg"
+import pirateShip from "../assets/monsters/pirate/ship.jpg"
 
 const monsters = [
+	{
+		type: "Pirate",
+		avatar: PirateImg,
+		minDmg: 5,
+		maxDmg: 12,
+		healthPoints: 70,
+		currentHealth: 70,
+		backgroundImg: pirateShip,
+		dodgeChance: 0,
+		phrases: [
+			'Ho-ho-ho',
+			'Where is my parrot?',
+		]
+	},
 	{
 		type: "Fat Palladin",
 		avatar: FatPalladinImg,
@@ -54,6 +71,7 @@ const monsters = [
 			'Feel the power of my sword!',
 			'Ave Caesar!',
 		]
-	}
+	},
+	
 ]
 export default monsters;
